02b644542e9ab613c9e66bbaa13ecc06deafa2d1
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-02-16 14:16:50 -0500

    irst commit

---
+++ js/add.js
@@ -0,0 +1,16 @@
+
+   //const greeter = "hey hi";
+    times = 4;
+x=2;
+y=5;
+
+    if (times > 3) {
+        let greeter = "say Hello instead"; 
+        console.log(greeter)
+        greeter= "hi";
+        console.log(greeter)
+        
+    }
+    
+    //console.log(greeter) // "say Hello instead"
+    console.log(greeter);
